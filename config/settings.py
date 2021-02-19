6ae73ba840733214214fba05d4f23d9adb461128
hojuly <hojune.choi.me@gmail.com>
2021-02-19 16:42:19 +0900

    apply drf

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'market.apps.MarketConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,5 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+MODEL_ROOT = os.path.join(BASE_DIR, 'models')
--- market/urls.py
+++ market/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
+from django.conf.urls import url, include
+
+from rest_framework import routers
+
+import market.serializers
 from .views import index
 
 
 app_name = 'market'
 
+router = routers.DefaultRouter()
+router.register('prices', market.serializers.PriceViewSet)
+
 urlpatterns = [
     path('', index, name='home'),
+
+    url(r'^api/v1/', include((router.urls, 'market'), namespace='api')),
 ]
