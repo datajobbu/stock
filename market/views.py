697cc7d10b74ca315d266fa39a59380b59b8b529
hojuly <hojune.choi.me@gmail.com>
2021-02-23 13:37:56 +0900

    add favicon

--- market/views.py
+++ market/views.py
@@ -1,9 +1,18 @@
+
+import os
+import json
+import datetime
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as pdr
+from tensorflow import keras
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Price
+from config.settings import MODEL_ROOT
 
-import json
 
 def index(request):
     prices = Price.objects.order_by('-date')[:30].values()
@@ -26,5 +35,48 @@ def index(request):
         'lows': low_list,
         'dates': json.dumps(date_list),
     }
-
+    predict()
     return render(request, 'market/home.html', context)
+
+
+def predict():
+    model = keras.models.load_model(os.path.join(MODEL_ROOT, 'lstm/'))
+    start = datetime.date.today() - datetime.timedelta(days=30)
+    end = datetime.date.today()
+
+    df = pdr.DataReader('005930.ks', 'yahoo', start, end)
+    high = df['High'].values
+
+    seq_len = 7
+
+    result = []
+    for idx in range(len(high) - seq_len + 1):
+        result.append(high[idx:idx + seq_len])
+
+    normalized = []
+    window_mean = []
+    window_std = []
+    for window in result:
+        normalized_window = [((p-np.mean(window))/np.std(window)) for p \
+                                in window]
+        normalized.append(normalized_window)
+        window_mean.append(np.mean(window))
+        window_std.append(np.std(window))
+
+    result = np.array(normalized)
+
+    x = np.reshape(result, (result.shape[0], result.shape[1], 1))
+    pred = model.predict(x)
+
+
+    real = []
+    pred_result = []
+    for i in range(len(pred)):
+        n1 = (pred[i]*window_std[i]) + window_mean[i]
+        n2 = (x[i]*window_std[i]) + window_mean[i]
+        pred_result.append(n1)
+        real.append(n2)
+
+    print(pred_result[-1])
+
+    
