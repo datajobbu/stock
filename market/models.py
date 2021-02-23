36d18a16c50ce7d5d66ce1ad072f57925831783d
hojuly <hojune.choi.me@gmail.com>
2021-02-23 15:19:30 +0900

    separtae apps: stocks

--- market/models.py
+++ market/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-# Create your models here.
-class Price(models.Model):
-    date = models.DateField()
-    start = models.IntegerField()
-    high = models.IntegerField()
-    low = models.IntegerField()
-    end = models.IntegerField()
-    volume = models.IntegerField()
+
+class Stocks(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=10)
